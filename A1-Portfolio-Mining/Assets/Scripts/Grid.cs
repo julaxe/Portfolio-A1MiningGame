e9d0b0b67be98820adb0597c930230d1f838e466
Julaxe <julaxe8@gmail.com>
2022-01-29 10:02:55 -0500

    now the grid is resizable and you can change the probabilities in the inspector for each tile

--- A1-Portfolio-Mining/Assets/Scripts/Grid.cs
+++ A1-Portfolio-Mining/Assets/Scripts/Grid.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SocialPlatforms;
+using UnityEngine.UI;
 
 public class Grid : MonoBehaviour
 {
@@ -10,14 +11,26 @@ public class Grid : MonoBehaviour
     public int size = 10; // 10 rows and 10 columns
 
     public GameObject[,] TileGrid;
+
+    private RectTransform _rectTransform;
+
+    private GridLayoutGroup _gridLayoutGroup;
+
+    [Header("Probabilities")] 
+    public int tier5 = 5;
+    public int tier4 = 15;
+    public int tier3 = 30;
+    public int tier2 = 30;
     // Start is called before the first frame update
     void Start()
     {
         _tilePrefab = Resources.Load<GameObject>("Prefabs/Tile");
+        _rectTransform = GetComponent<RectTransform>();
+        _gridLayoutGroup = GetComponent<GridLayoutGroup>();
         TileGrid = new GameObject[size, size];
         
-        CreateGrid();
-        SetupNeighbors();
+        StartCoroutine(SetupGrid());
+        
     }
 
     void CreateGrid()
@@ -93,24 +106,38 @@ public class Grid : MonoBehaviour
         //2 -> 30%
         //1 -> 20%
         int randomNumber = Random.Range(0, 100);
-        if (randomNumber < 5)
+        if (randomNumber < tier5)
         {
             return 5;
         }
-        if (randomNumber < 20)
+        if (randomNumber < (tier5 + tier4))
         {
             return 4;
         }
-        if (randomNumber < 50)
+        if (randomNumber < (tier5 + tier4 + tier3))
         {
             return 3;
         }
-        if (randomNumber < 80)
+        if (randomNumber < (tier5 + tier4 + tier3 + tier2))
         {
             return 2;
         }
 
         return 1;
+    }
+
+    public IEnumerator SetupGrid()
+    {
+        yield return new WaitForEndOfFrame(); // so we can get the actual width of the RectTransform
+       
+        //resizing the tiles in the grid
+        float width = _rectTransform.sizeDelta.x;
+        float sizeOfTiles = width / size;
+        _gridLayoutGroup.cellSize = new Vector2(sizeOfTiles, sizeOfTiles);
+        
+        CreateGrid();
+        SetupNeighbors();
+
     }
 
 }
